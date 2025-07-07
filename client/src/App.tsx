4ff21707c96d006a2c5195eaf100a2f5ab61f399
adilkadivala <adilkadivala560@gmail.com>
2025-07-07 08:06:21 +0530

    project structure is under development

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <p className="text-rose-500">hello we have started</p>
+    </div>
+  );
+}
+
+export default App;
